3a3f4c9d4934f6a2edd29530812a3ffdbd56c8df
hikee9123 <hikee9123@gmail.com>
2021-08-13 22:51:36 +0900

    dd

--- selfdrive/ui/kegman_ui.h
+++ selfdrive/ui/kegman_ui.h
@@ -211,7 +211,7 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
     else {
       snprintf(val_str, sizeof(val_str), "%.2f", (gpsAccuracyUblox));
     }
-    snprintf(uom_str, sizeof(uom_str), "%d", altitudeUblox);
+    snprintf(uom_str, sizeof(uom_str), "%.1f", altitudeUblox);
     bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "GPS PREC",
         bb_rx, bb_ry, bb_uom_dx,
         val_color, lab_color, uom_color,
