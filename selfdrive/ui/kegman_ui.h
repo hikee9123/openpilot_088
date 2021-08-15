dc946b0cc35f7011d7346b0550a22f28136f5bc2
hikee9123 <hikee9123@gmail.com>
2021-08-15 15:31:36 +0900

    dd

--- selfdrive/ui/kegman_ui.h
+++ selfdrive/ui/kegman_ui.h
@@ -496,10 +496,10 @@ static void bb_draw_tpms(UIState *s, int viz_tpms_x, int viz_tpms_y) {
 
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
   int pos_x = viz_tpms_x + (viz_tpms_w / 2);
-  int pos_y = viz_tpms_y - 15;
+  int pos_y = viz_tpms_y - 10;
   bb_ui_text(s, pos_x, pos_y, "TPMS(psi)", 15, COLOR_WHITE_ALPHA(180), "sans-regular");
 
-  pos_y = viz_tpms_y + 35;
+  pos_y = viz_tpms_y + 30;
   print_tpms( s, pos_x-55, pos_y+50, fl );
   print_tpms( s, pos_x+55, pos_y+50, fr );
   print_tpms( s, pos_x-55, pos_y+100, rl );
